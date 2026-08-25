f0f30ab415031af336972185fbb5d074aaaead8b
mariohit <gilbertmario.hitimana@gmail.com>
2026-08-25 21:03:39 +0200

    deploy

--- mialigo/app/page.tsx
+++ mialigo/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
 
   const config = {
     name: "Mario",
-    bio: "Building in public 🚀",
+    bio: "Building in publics 🚀",
     links: [
       {
         title: "Instagram",
