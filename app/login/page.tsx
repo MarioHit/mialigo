489e8fc6923f7873d21033e6414c0e9b150cdf14
mariohit <gilbertmario.hitimana@gmail.com>
2026-08-27 20:00:10 +0200

    link ok to dashboard

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -33,7 +33,7 @@ export default function LoginPage() {
       const { error } = await supabaseClient.auth.signInWithOtp({
         email,
         options: {
-          emailRedirectTo: `${window.location.origin}/auth/callback`,
+          emailRedirectTo: `${window.location.origin}/dashboard`,
         },
       });
 
